1b7e7c87fc7e8188c79c5b6e82956bef9c28dc27
Idlir <idlirbregu02@gmail.com>
2024-11-14 10:07:22 +0100

    first commit

--- app/src/main/java/com/example/citamaster/MainActivity.java
+++ app/src/main/java/com/example/citamaster/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
