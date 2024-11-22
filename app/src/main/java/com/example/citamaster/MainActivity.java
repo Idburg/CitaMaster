f6d5190702fec3054b588a680adaf9b4b51453b7
Idlir <idlirbregu02@gmail.com>
2024-11-22 12:30:34 +0100

    1

--- app/src/main/java/com/example/citamaster/MainActivity.java
+++ app/src/main/java/com/example/citamaster/MainActivity.java
@@ -8,8 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
-    
+    Calendar fecha;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        fecha = Calendar.getInstance();
 
 
     }
